407d7b9110095cd776f726dfa3b8aee8d1a69716
achraf <achraf.benalaya@iit.ens.tn>
2017-02-14 01:17:16 +0100

    firstcomit

---
+++ UnittestXamarin/UnittestXamarin/viewModels/MathviewModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace UnittestXamarin.viewModels
+{
+   public class MathviewModel :INotifyPropertyChanged
+    {
+
+        private int   _VA { get; set; }
+
+        public int VA
+        {
+            get { return _VA; }
+            set
+            {
+                _VA = value;
+                
+                OnPropertyChanged();
+            }
+
+
+
+        }
+
+        private int _Vb { get; set; }
+
+        public int Vb
+        {
+            get { return _Vb; }
+            set
+            {
+                _Vb = value;
+
+                OnPropertyChanged();
+            }
+
+
+
+        }
+
+
+        private int _res { get; set; }
+
+        public int res
+        {
+
+            get { return _res; }
+            set
+            {
+                _res = value;
+
+                OnPropertyChanged();
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+        public ICommand AddCommand
+
+        {
+
+
+            get
+            {
+
+                return new Command(() =>
+
+                {
+                    res = _VA + _Vb ;
+                }
+
+            );
+
+        }
+        }
+
+
+
+
+        public ICommand Multiply
+
+        {
+
+
+            get
+            {
+
+                return new Command(() =>
+
+                {
+                    res = _VA * _Vb;
+                }
+
+            );
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+    }
+}
---
+++ myappunittest/mathunittest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using  UnittestXamarin.viewModels;
+namespace myappunittest
+{
+    [TestClass]
+    public class mathunittest
+    {
+        [TestMethod]
+        [TestCategory("add section")]
+        public void add_4_to_6_return_10_test()
+        {
+
+            //arrange
+
+
+            var vm = new MathviewModel();
+            vm.VA = 4;
+            vm.Vb = 6;
+
+            //act
+
+            vm.AddCommand.Execute(null);
+
+
+
+            //assert
+            Assert.IsTrue(vm.res==10," should be vm.res==10");
+
+
+        }
+
+
+
+
+
+
+
+        [TestMethod]
+        [TestCategory("multiplycommand")]
+        public void multi_4_by_6_return_24_test()
+        {
+
+            //arrange
+
+
+            var vm = new MathviewModel();
+            vm.VA = 4;
+            vm.Vb = 6;
+
+            //act
+
+            vm.Multiply.Execute(null);
+
+
+
+            //assert
+           // Assert.IsTrue(vm.res == 24, " should be vm.res==24");
+           Assert.AreEqual(24,vm.res, "should be vm.res==24");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
